7f82376bb871c8434f987d6a6acef325383ad8d8
Jake Whatley <jacob.whatley91@gmail.com>
2015-02-11 19:48:45 -0500

    samsung 8974-common: common sepolicy bringup for android-5.0
    Change-Id: I0a6733330049c14dec534abba25453289e92d65e

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -64,3 +64,9 @@ BOARD_USES_QC_TIME_SERVICES := true
 BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 BOARD_VOLD_MAX_PARTITIONS := 28
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
+
+include device/qcom/sepolicy/sepolicy.mk
+
+BOARD_SEPOLICY_DIRS += \
+    $(COMMON_PATH)/sepolicy
+
